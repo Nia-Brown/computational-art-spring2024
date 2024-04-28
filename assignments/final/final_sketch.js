2432f241e6171fa2bdd9ea8857ab0b4d6aceafde
Nia-Brown <97653024+Nia-Brown@users.noreply.github.com>
2024-04-28 15:42:08 -0400

    NOT FINISHED
    Not finished yet. Just need assistance with image displays and fixing some small bugs when trying to return to the original gallery.

--- assignments/final/final_sketch.js
+++ assignments/final/final_sketch.js
@@ -1,145 +1,119 @@
-// Individual sketch functions for each portrait
-let sketchAudioVisualizer = function (p) {
-    p.setup = function () {
-        let canvas = p.createCanvas(500, 300);
-        canvas.parent('audio-visualizer'); // Position canvas within the div
-        audioVisualizer = new AudioVisualizer(p);
-    };
-
-    p.draw = function () {
-        p.background(255); // change maybe
-        if (playing) {
-            console.log('playing:', playing); // Check playing state
-            let spectrum = fft.analyze();
-            console.log(spectrum); // Check spectrum data
-            audioVisualizer.update(spectrum);
-            displayBars(p);
-        }
-
-        audioVisualizer.display();
-
-        // Update playback speed based on mouse movement
-        audioVisualizer.setPlaybackSpeed(p.mouseX / p.width);
-    };
-};
-
-let sketchFractalGarden = function (p) {
-    p.setup = function () {
-        let canvas = p.createCanvas(500, 300);
-        canvas.parent('fractal-garden');
-        FractalGarden = new FractalGarden(p);
-    };
-
-    p.draw = function () {
-        p.background(77, 76, 75);
-        FractalGarden.update();
-        FractalGarden.display();
-    };
-};
-
-// Similar sketch functions for particleSymphony, perlinPortrait, and cellularCity
-
-// Global variables (outside of any sketch function)
-let song;
-let fft;
-let playing = false;
-let playButton;
-let audioVisualizer;
+// State Manager
+let stateManager;
+let originalState;
 
 function preload() {
-    song = loadSound('../final/sounds/dream.mp3'); // Replace with your audio path
+  originalState = new OriginalState();
+  // Preload images
+  originalState.loadImages();
 }
 
 function setup() {
-    fft = new p5.FFT();
-
-    playButton = createButton('Play');
-    playButton.position(width / 2 - 30, height + 20);
-    playButton.mousePressed(toggleSound);
-
-    // Create new p5 instances for each sketch function
-    new p5(sketchAudioVisualizer);
-    new p5(sketchFractalGarden);
-    // ... create p5 instances for other sketch functions
+  createCanvas(2500, 600);
+  stateManager = new StateManager(originalState);
 }
 
-function toggleSound() {
-    if (song.isPlaying()) {
-        song.pause();
-        playing = false;
-        playButton.html('Play');
-    } else {
-        song.loop();
-        playing = true;
-        playButton.html('Pause');
-    }
+function resetButton() {
+  let button = createButton('Return to Gallery');
+  button.position(width / 2 - 50, height + 20);
+  button.mousePressed(() => {
+    stateManager.switchState(originalState);
+  });
 }
 
-let currentFrame = 0; // Track playback position
+function draw() {
+  stateManager.currentState.draw();
+  resetButton();
+}
 
-// AudioVisualizer class (modified to accept spectrum data)
-class AudioVisualizer {
-    constructor(p) {
-        this.fft = new p5.FFT();
-        this.isPlaying = false;
-        this.p = p; // Reference to the p5 instance
-        this.bars = []; // Array to store bar information
-        this.playbackSpeed = 1.0; // Initial playback speed
-    }
+function mouseClicked() {
+  stateManager.handleMouseClick();
+}
 
-    update(spectrum) {
-        if (this.isPlaying) {
-            console.log('playing:', playing); // Check playing state
-            console.log(spectrum); // Check spectrum data
+// States
+class OriginalState {
+  constructor() {
+    this.boxWidth = 500;
+    this.boxHeight = 300;
+    this.spacing = (width - (this.boxWidth * 5)) / 6;
+    this.images = [];
+  }
 
-            this.bars = []; // Clear previous bars
-            for (let i = 0; i < spectrum.length; i++) {
-                let amp = spectrum[i];
-                let barHeight = this.p.map(amp, 0, 256, 0, this.p.height);
-                let barColor = this.p.color(0, 255, 0); // Bright green
-                console.log(barColor);
-                this.bars.push({ height: barHeight, color: barColor });
-            }
-            console.log(this.bars); // Check bars array content
+  loadImages() {
+    // Load images into the array
+    this.images.push(loadImage('../final_images/soundwaves.jpg'));
+    // this.images.push(loadImage('path_to_fractal_garden_image.jpg'));
+    // Load more images as needed
+  }
+
+  draw() {
+    for (let i = 0; i < 5; i++) {
+      let x = this.spacing + (this.boxWidth + this.spacing) * i;
+      let y = 0;
+
+      // Draw box
+      fill(200);
+      rect(x, y, this.boxWidth, this.boxHeight);
+
+      // Display preloaded image
+      if (this.images[i]) {
+        image(this.images[i], x, y, this.boxWidth, this.boxHeight);
+      }
+
+      // Check if mouse is over the box
+      if (mouseX > x && mouseX < x + this.boxWidth && mouseY > y && mouseY < y + this.boxHeight) {
+        cursor(HAND);
+        if (mouseIsPressed) {
+          // Switch to corresponding state based on index (i)
+          switch (i) {
+            case 0:
+              stateManager.switchState(new AudioVisualizerState());
+              break;
+            case 1:
+              stateManager.switchState(new FractalGardenState());
+              break;
+            // Add more cases for other states
+            default:
+              console.warn("Unknown box index:", i);
+          }
         }
-
-        currentFrame += this.playbackSpeed; // Advance playback position
-
-        // Schedule next update based on adjusted speed
-        setTimeout(() => this.update(), 1000 / (this.playbackSpeed * song.duration()));
+      }
     }
-
-
-    display(p) {
-        
-        if (this.isPlaying) {
-            for (let i = 0; i < this.bars.length; i++) {
-                this.p.fill(this.bars[i].color);
-                // Check calculated color values (optional)
-                // console.log(this.bars[i].color);
-                console.log('Drawing bar:', i);
-                p.rect(i * this.p.width / this.bars.length, this.p.height, this.p.width / this.bars.length * 5, -barHeight); // Increased width by factor of 5
-            }
-        }
-    }
-
-    setPlaybackSpeed(speed) {
-        this.playbackSpeed = speed;
-    }
+  }
 }
 
+class AudioVisualizerState {
+  draw() {
+    // Draw audio visualizer
+    background(255);
+    // Add audio visualizer drawing logic here
+  }
+}
 
-// Implement similar classes for FractalGarden, ParticleSymphony, etc.
-class FractalGarden {
-    constructor(p) {
+class FractalGardenState {
+  draw() {
+    // Draw fractal garden
+    background(77, 76, 75);
+    // Add fractal garden drawing logic here
+  }
+}
 
-    }
+// State Manager
+class StateManager {
+  constructor(initialState) {
+    this.states = {};
+    this.currentState = initialState;
+  }
 
-    update() {
+  addState(name, state) {
+    this.states[name] = state;
+  }
 
-    }
+  switchState(newState) {
+    this.currentState = newState;
+  }
 
-    display() {
-
-    }
-}  
+  handleMouseClick() {
+    // No need to implement this in the manager since it's handled in OriginalState
+  }
+}
